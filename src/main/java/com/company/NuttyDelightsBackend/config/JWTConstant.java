56b2f833e37a2b1dc0b6a94b759c13a830e66bad
Anant Singhal <anantsinghal47@gmail.com>
2023-09-14 09:44:49 +0530

    some changes in project

--- src/main/java/com/company/NuttyDelightsBackend/config/JWTConstant.java
+++ src/main/java/com/company/NuttyDelightsBackend/config/JWTConstant.java
@@ -1,6 +1,6 @@
 package com.company.NuttyDelightsBackend.config;
 
 public class JWTConstant {
-    public static  final  String SECRET_KEY = "daxoahwpwqdpliosghjiwyhd44665lpoz6974544aqlpal8975";
+    public static  final  String SECRET_KEY = "daxoahwpwqdpliosghjiwyhd44665lpoz6974544aqlpal8975kdoahhdaohdh4545ahdhadhahdkjadjhadjhajdjiadjiadj8975145127488";
     public static  final  String JWT_HEADER= "Authorization";
 }
